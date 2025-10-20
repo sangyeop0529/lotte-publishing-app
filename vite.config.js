81100d78144ec1307d4ce56f2adaa22de2eb6e39
sangyeop0529 <sangyeop0529@gmail.com>
2025-10-20 17:09:31 +0900

    vite 설정 변경

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // 외부 접속 허용
+    port: 5173,
+    hmr: {
+      host: "localhost", // 또는 실제 IP
+      protocol: "ws",
+    },
+  },
+});
