266506d72cc1eb8e76f13601ee898899e5ebfaa1
sangyeop0529 <sangyeop0529@gmail.com>
2025-10-20 17:11:04 +0900

    롤백

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // 외부 접속 허용
-    port: 5173,
-    hmr: {
-      host: "localhost", // 또는 실제 IP
-      protocol: "ws",
-    },
-  },
 });
